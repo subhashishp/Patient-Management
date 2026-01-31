39cddba936b21b8aa522f1d3524e20155c09f6e7
subhashishp <subhashishpaul13@gmail.com>
2026-02-01 01:03:32 +0530

    billing service docker contained

--- billingservice/src/main/java/com/patientmanagement/billingservice/grpc/BillingGrpcService.java
+++ billingservice/src/main/java/com/patientmanagement/billingservice/grpc/BillingGrpcService.java
@@ -1,4 +1,33 @@
 package com.patientmanagement.billingservice.grpc;
 
-public class BillingGrpcService {
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+
+        // Business logic here
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);  // send response from grpc
+        responseObserver.onCompleted();     // end the cycle of response
+    }
 }
