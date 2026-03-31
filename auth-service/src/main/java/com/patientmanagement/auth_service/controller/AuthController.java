8c7436203ae14d7bb90f1fd04cc3ba9df5f44549
subhashishp <subhashishpaul13@gmail.com>
2026-03-31 13:09:58 +0530

    add authentication service

---
+++ auth-service/src/main/java/com/patientmanagement/auth_service/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.patientmanagement.auth_service.controller;
+
+import com.patientmanagement.auth_service.dto.LoginRequestDTO;
+import com.patientmanagement.auth_service.dto.LoginResponseDTO;
+import com.patientmanagement.auth_service.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+
+        if(tokenOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+
+}
